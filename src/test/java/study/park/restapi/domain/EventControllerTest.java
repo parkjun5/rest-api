c3a471eca5fcc8c1a4e02c2acb610ca4940b0dfa
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2022-08-10 22:31:22 +0900

    fail on unknown properties 설정 추가
    spring.jackson.deserialization 설정으로 api의 입력값을 관리

--- src/test/java/study/park/restapi/domain/EventControllerTest.java
+++ src/test/java/study/park/restapi/domain/EventControllerTest.java
@@ -2,6 +2,7 @@ package study.park.restapi.domain;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import study.park.restapi.dto.EventDto;
 import study.park.restapi.repository.EventRepository;
 
 import java.time.LocalDateTime;
@@ -34,7 +36,38 @@ class EventControllerTest {
     ObjectMapper objectMapper;
 
     @Test
+    @DisplayName("기본 테스트 POST /api/events/")
     void createEvent() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 8, 11, 18, 0, 0))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 8, 12, 18, 0, 0))
+                .beginEventDateTime(LocalDateTime.of(2022, 8, 11, 18, 0, 0))
+                .endEventDateTime(LocalDateTime.of(2022, 8, 12, 18, 0, 0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("신사역 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(eventDto)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE + ";charset=utf8"))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    @DisplayName("예외 테스트 POST /api/events/")
+    void createEventInputException() throws Exception {
         Event event = Event.builder()
                 .name("Spring")
                 .id(100)
@@ -52,21 +85,13 @@ class EventControllerTest {
                 .eventStatus(EventStatus.PUBLISHED)
                 .build();
 
+
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaTypes.HAL_JSON)
-                        .content(objectMapper.writeValueAsString(event))
-                )
+                        .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE + ";charset=utf8"))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT));
+                .andExpect(status().isBadRequest());
 
-    }
-
-
+        }
 }
