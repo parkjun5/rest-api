e19560eb9ea874623960d96bd4069cbcaccc1812
박세준 <sjpark@selab.co.kr>
2022-08-12 15:46:43 +0900

    test 환경 분리

--- src/test/java/study/park/restapi/RestApiApplicationTests.java
+++ src/test/java/study/park/restapi/RestApiApplicationTests.java
@@ -2,10 +2,12 @@ package study.park.restapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RestApiApplicationTests {
 
     @Test
--- src/test/java/study/park/restapi/domain/EventControllerTest.java
+++ src/test/java/study/park/restapi/domain/EventControllerTest.java
@@ -9,9 +9,11 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 class EventControllerTest {
 
     @Autowired
